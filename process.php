4456cd8f8b0153873ce8453e97097e43f33f5328
Aung Ko Ko <aungkokomyintoo@gmail.com>
2017-01-18 14:24:24 +0630

    Updates

--- process.php
+++ process.php
@@ -1,31 +1,60 @@
 <?php
  
+// $data=[];
+   
+$data=[
+    'name' => $_POST['name'],
+    'email' => $_POST['email'],
+    'checkbox' => $_POST['checkbox'],
+    'email_to'=> "you@yourdomain.com",
+    'email_subject'=>$_POST['subject'],
+  ];
 
 call_user_func(function(){
-  $data=[
-  'name' => $_POST['name'],
-  'email' => $_POST['email'],
-  'image' => isset($_FILES['image']['tmp_name']),
-  'email_to'=> "you@yourdomain.com",
-  'email_subject'=>isset($_POST['subject']),
-];
 
-
-// $data=array_filter($data);
-// echo $data['image'];
-// var_dump($data);
-
-confirmation($data);
-
-
+  if(isset($_FILES['image'])){
+    if(!file_upload()){
+      die('11');
+    }
+   }
+  prepare();
+  
 });
+
+
+function prepare(){
+  // echo "prepare";
+
+  $data=$GLOBALS['data'];
+  foreach ($data['checkbox'] as $value) {
+    echo $value;
+  }
+
+
+  // var_dump($GLOBALS['data']);
+}
+
+function file_upload(){
+  $target_dir = "uploads/";
+  $target_file = $target_dir . uniqid()."_".basename($_FILES["image"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+  // Check if image file is a actual image or fake image
+      $check = getimagesize($_FILES["image"]["tmp_name"]);
+      if($check !== false) {
+      
+        move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+        $GLOBALS['data']['image']=$target_file;
+        return true;
+      } else {
+          echo "File is not an image.";
+          return false;
+      }
+  
+}
 
 function confirmation($data){
   include('confirmation.php');
-
-  if(isset($_POST['submit'])){
-    send_email($data);
-  }
 
 }
 
